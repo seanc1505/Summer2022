e32f434f6b9ca0e22691088fbe8749ec12cacc00
scahill2 <sean.cahill@analog.com>
2022-05-08 23:07:38 -0400

    Finished importsort functions, moved to utilities

---
+++ utilities/checkvaliddir.py
@@ -0,0 +1,22 @@
+# Check if the directory is valid, open dialog box if not
+# returns directory path
+
+import os
+from tkinter import *
+from tkinter import filedialog
+
+
+def checkValidDirectory(dir, check_name, make_if_fail=True):
+    print("Checking for " + check_name + " at: "+dir)
+    if os.path.exists(dir):
+        pass
+    elif dir == "" or make_if_fail == False:
+        print("Could not find directory; " + dir + ", \r Please select it")
+        root = Tk()
+        dir = filedialog.askdirectory()
+        root.destroy()
+    else:
+        print("Could not find directory; " + dir + ", creating")
+        os.mkdir(dir)
+    print("Directory selected: " + dir)
+    return dir
---
+++ utilities/goprorename.py
@@ -0,0 +1,9 @@
+def goproRename(file_name):
+    if file_name[:2] != "GH":
+        print("Not a gopro file: " + file_name)
+        return file_name
+
+    if file_name[-7] != "_":
+                chapter = "_" + file_name[2:4]
+                file_name = file_name[:2] + file_name[4:-4] +  chapter +".mp4"
+    return file_name
