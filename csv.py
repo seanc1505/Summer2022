7e040e66be4842639279fe7e56968eede99a9029
scahill2 <sean.cahill@analog.com>
2022-05-25 16:57:13 -0700

    adding basic csv template and syntax on readme

---
+++ csv.py
@@ -0,0 +1,15 @@
+class Csv():
+    def __init__(self) -> None:
+        pass
+
+    def importCsv(self):
+        print("import csv")
+
+    def createCsvManual(self):
+        print("createCsvManual")
+
+    def createCsvAlgorithim(self):
+        print("createCsvAlgorithim")
+
+    def processCsv(self):
+        print("processCsv")
