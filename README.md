77e86d0fb987114fbedbfa8d43d3607406a525b3
seanc1505 <54627825+seanc1505@users.noreply.github.com>
2022-06-12 00:47:14 +0100

    Added export video from subclip generation

--- README.md
+++ README.md
@@ -18,6 +18,42 @@ Current Active branches:
 
 * Main.py will be main file that pulls everything together
 
+# Video Editor
+
+## Statement of work
+3 classes created  
+Video
+SourceVideo(Video)
+ExportVideo(Video)
+### Video
+Definition of common vars
+
+### Source Video
+- [ ] Init -> pass a path and update source path vars
+- [ ] Import Videos -> Move files to specified directory in format (YY_MM_DD)
+- [ ] Rename files -> Renames go pro files from GHYYXXXX to GHXXXX_YY
+
+
+
+### Export Video
+- [x] Create dictionary from csv file
+- [ ] export clips
+- [ ] naming of files and sorting of source and export directories
+- [ ] Rearrange the export to call subclip_dict and pass in csv there
+- [ ] updating of documentation
+
+## Added functionality
+
+
+
+# Functionality concepts
+
+Detailing the desired structure and functionality of the project as a whole.
+Should be an a fully automated editing process navigated by an interactive online GUI.
+Interacts with machine learning progams hosted on the cloud. Computation should probably be done on the cloud too but may require upload which would be slow.
+
+## Layout of code
+
 ### Objects
 
 * GUI
--- video/export_video.py
+++ video/export_video.py
@@ -1,6 +1,7 @@
 from video.video import Video
 from utilities.checkvaliddir import checkValidDirectory
 import moviepy
+from moviepy.video.compositing.concatenate import concatenate_videoclips
 import os
 
 
@@ -9,29 +10,45 @@ class ExportVideo(Video):
         super().__init__(default_settings)
         self.video_path = path
 
-    def exportVideo(self, subclip_dict, name):
+    def exportVideo(self, name):
         print("export video")
-        for video in subclip_dict:
-            video_clip = moviepy.VideoFileClip((self.video_path+"/"+video))
-    
-    def prepDictionary(self,dataframe):
+        for paddler in self.video_clips_dict:
+            subclip_list = []
+            for video in self.video_clips_dict[paddler]:
+                video_clip = moviepy.VideoFileClip(
+                            ( self.video_path+"/"+video))
+                for subclip in self.video_clips_dict[paddler][video]:
+                    temp_dict = self.video_clips_dict[paddler][video][subclip]
+                    print(video)
+                    print(temp_dict)
+                    current_subclip = video_clip.subclip(
+                                        temp_dict["Start Time"], temp_dict["Stop Time"])
+                    subclip_list.append(current_subclip)
             
+            final_clip = concatenate_videoclips(subclip_list)
+            video_name = self.video_path  +"/"+name +"_"+ paddler + self.video_date_created + ".mp4"
+            final_clip.write_videofile(video_name)
+    
+    def prepDictionary(self,dataframe):       
         clip_time_dict = {}
         video_name_dict={}
         video_number_dict = {}
         self.video_clips_dict = {}
         prev_row = ""
         for index,row in dataframe.iterrows():
+            
             # if index > 4: 
             #     break
-            clip_time_dict['Start Time'] = row['Start Time']
-            clip_time_dict['Stop Time'] = row['Stop Time']
-            video_number_dict[row['Subclip Num']]=clip_time_dict.copy()
-            if index >0:
+            if index > 0:
                 if row['Subclip Num'] == 0:
                     video_name_dict[prev_row['Video Name']] = video_number_dict.copy()
                     video_number_dict = {}
-                
+            
+            clip_time_dict['Start Time'] = row['Start Time']
+            clip_time_dict['Stop Time'] = row['Stop Time']
+            video_number_dict[row['Subclip Num']]=clip_time_dict.copy()
+
+            if index >0:
                 if prev_row['Paddler'] != row['Paddler']:
                     self.video_clips_dict[prev_row['Paddler']] = video_name_dict.copy()
                     video_name_dict = {}
