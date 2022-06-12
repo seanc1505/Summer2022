34154afd8170ed8db198207aa14aca1f07be45b0
seanc1505 <54627825+seanc1505@users.noreply.github.com>
2022-06-12 22:11:24 +0100

    updates to default settings

--- defaults.py
+++ defaults.py
@@ -3,26 +3,26 @@ import json
 
 class Defaults():
     def __init__(self) -> None:
-        self.ImportDefaults()
+        self.ImportDefaults(user=True)
 
-    def ImportDefaults(self, user="user"):
-        if user != "user":
+    def ImportDefaults(self, user=True):
+        if user != True:
             with open('default_settings.json') as json_file:
                 self.default_settings = json.load(json_file)
         else:
             with open('user_settings.json') as json_file:
                 self.user_settings = json.load(json_file)
 
-    def ResetDefaults(self):
-        self.ImportDefaults(user="defaults")
+    def resetDefaults(self):
+        self.ImportDefaults(user=False)
         with open('user_settings.json', 'w') as outfile:
             json.dump(self.default_settings, outfile)
 
-    def EditDefaults(self, edits):
+    def editDefaults(self, edits):
         for value in edits:
             self.user_settings[value] = edits[value]
         # Implement editing/saving within a GUI using values passed
 
-    def SaveDefaults(self):
+    def saveDefaults(self):
         with open('user_settings.json', 'w') as outfile:
             json.dump(self.user_settings, outfile)
