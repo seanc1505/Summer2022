e8404328553914c34929470fd90c3f4d22849038
seanc1505 <54627825+seanc1505@users.noreply.github.com>
2022-06-12 22:12:58 +0100

    updates to utilities to remove text

--- utilities/checkvaliddir.py
+++ utilities/checkvaliddir.py
@@ -6,18 +6,13 @@ from tkinter import *
 from tkinter import filedialog
 
 
-def checkValidDirectory(dir="", check_name="", make_if_fail=True):
-    if check_name != "":
-        print("Checking for " + check_name + " at: "+dir)
+def checkValidDirectory(dir="", make_if_fail=True):
     if os.path.exists(dir):
         pass
     elif dir == "" or make_if_fail == False:
-        print("Could not find directory; " + dir + ", \r Please select it")
         root = Tk()
         dir = filedialog.askdirectory()
         root.destroy()
     else:
-        print("Could not find directory; " + dir + ", creating")
         os.mkdir(dir)
-    print("Directory selected: " + dir)
     return dir
--- utilities/checkvalidfile.py
+++ utilities/checkvalidfile.py
@@ -6,15 +6,11 @@ from tkinter import *
 from tkinter import filedialog
 
 
-def checkValidFile(file="", check_name=""):
-    if check_name != "":
-        print("Checking for " + check_name + " at: "+file)
+def checkValidFile(file=""):
     if os.path.exists(file):
         pass
     else:
-        print("Could not find file; " + file + ", \r Please select it")
         root = Tk()
         file = filedialog.askopenfilename()
         root.destroy()
-    print("File selected: " + file)
     return file
