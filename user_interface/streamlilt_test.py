752cfbcea6f32958c06b60ec174b112efa781922
seanc1505 <54627825+seanc1505@users.noreply.github.com>
2022-09-20 22:29:04 +0100

    clearing of files to focus on UI section

---
+++ user_interface/streamlilt_test.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.read_csv(  "C:/Users/seanc/Documents/0_code/Summer2022/test/22_07_04_streamlit_test.csv")
+
+st.title("Test page of a sample dataset")
+
+st.write("Here is the dataset sample:") 
+
+st.write(df)
