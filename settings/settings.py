32554a50cc242b369d39eebe106f26e421cbb5d5
seanc1505 <54627825+seanc1505@users.noreply.github.com>
2022-09-22 17:34:37 +0100

    defaults/

--- settings/settings.py
+++ settings/settings.py
@@ -2,34 +2,37 @@ import json
 from tkinter.filedialog import SaveFileDialog
 
 
-class Defaults():
+class Settings():
     def __init__(self) -> None:
-        self.importDefaults()
+        self.importSettings()
 
-    def importDefaults(self, user="user"):
-        """Imports defaults from user_settings.json or default_settings.son
+    def importSettings(self, user="user"):
+        """Imports Settings from user_settings.json or default_settings.son
             Stores within default settings"""
         if user != "user":
             with open('default_settings.json') as json_file:
                 self.default_settings = json.load(json_file)
+                self.settings = self.default_settings
         else:
             with open('user_settings.json') as json_file:
                 self.user_settings = json.load(json_file)
+                self.settings = self.user_settings
+        
 
-    def resetDefaults(self):
+    def resetSettings(self):
         """Sets user settings to default settings"""
-        self.ImportDefaults(user="defaults")
+        self.ImportSettings(user="Settings")
         with open('user_settings.json', 'w') as outfile:
             json.dump(self.default_settings, outfile)
 
-    def editDefaults(self, edits):
+    def editSettings(self, edits):
         """Adjusts user_settings to the information included in edits dicitonary"""
         for value in edits:
             self.user_settings[value] = edits[value]
-        self.SaveDefaults()
+        self.SaveSettings()
         # Implement editing/saving within a GUI using values passed
 
-    def saveDefaults(self):
+    def saveSettings(self):
         """Saves changed settings to user_settings.json"""
         with open('user_settings.json', 'w') as outfile:
             json.dump(self.user_settings, outfile)
